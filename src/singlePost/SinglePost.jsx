df895e12efe3361129e2c219767b98bd91f05ff3
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2021-11-17 04:13:54 +0530

    Update SinglePost.jsx

--- src/singlePost/SinglePost.jsx
+++ src/singlePost/SinglePost.jsx
@@ -17,7 +17,7 @@ export default function SinglePost() {
     const [updateMode, setUpdateMode] = useState(false);
     useEffect(() => {
         const getPost = async () => {
-            const res = await axios.get("/posts/" + path);
+            const res = await axios.get("https://letusblogg.herokuapp.com/api/posts/" + path);
             setPost(res.data);
             setTitle(res.data.title);
             setDesc(res.data.desc);
